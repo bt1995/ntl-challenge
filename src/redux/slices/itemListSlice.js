b7b7ad8576440b9dd3f82fbc6096b5793de5cc8f
Binay Timilsina <binay.timilsina@danelfin.com>
2023-07-24 22:11:59 +0200

    Removed unused reducers

--- src/redux/slices/itemListSlice.js
+++ src/redux/slices/itemListSlice.js
@@ -1,9 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    loading: null,
-    itemList: null,
-    error:null,
     itemCount: localStorage.getItem('cartListAmount')
 }
 
@@ -11,21 +8,12 @@ const itemListSlice = createSlice({
     name: 'itemList',
     initialState,
     reducers: {
-        setLoading: (state, action) => {
-            state.loading = action.payload
-        },
-        setItemList: (state, action) => {
-            state.itemList = action.payload
-        },
-        setError: (state, action) => {
-            state.error = action.payload
-        },
         setItemCount: (state, action) => {
             state.itemCount = action.payload
         },
     }
 })
 
-export const { setLoading, setError, setItemList, setItemCount } = itemListSlice.actions
+export const { setItemCount } = itemListSlice.actions
 
 export default itemListSlice.reducer
